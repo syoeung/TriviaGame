92ffd2ffa16e8157c65f2534747176f968dbc48a
Steven <yoeung@outlook.com>
2018-06-15 21:33:04 -0400

    changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var triviaQuestions = [{
+	question: "How many planets are in the Solar System?",
+	answerList: ["7", "8", "9", "10"],
+	answer: 2
+},{
+	question: " What is the largest planet in our solar system? ",
+	answerList: ["PLuto", "The Sun", "Jupiter", "Saturn"],
+	answer: 3
+},{
+	question: " What is the smallest planet in our solar system? ",
+	answerList: ["Venus", "Pluto", "Mercury", "Mars"],
+	answer: 3
+},{
+	question: "What planet in this solar system is most suitable for life",
+	answerList: ["Neptune", "Midgard", "Mars", "Earth"],
+	answer: 4
+},{
+    question: "What roots did the name of the planets come from? ",
+	answerList: ["Greek", "Roman", "Egpytian", "Buddist"],
+	answer: 4
+
+
+
+
+}];
