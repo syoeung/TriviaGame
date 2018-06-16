f4f918197642fe004b020036a103f3f9cfda1ad6
Steven <yoeung@outlook.com>
2018-06-16 00:55:18 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 var triviaQuestions = [{
-	question: "How many planets are in the Solar System?",
+	question: "How many planets are in the Solar System ?",
 	answerList: ["7", "8", "9", "10"],
 	answer: 1
 },{
-	question: " What is the largest planet in our solar system? ",
-	answerList: ["PLuto", "The Sun", "Jupiter", "Saturn"],
+	question: " What is the largest planet in our solar system ? ",
+	answerList: ["Pluto", "The Sun", "Jupiter", "Saturn"],
 	answer: 2
 },{
 	question: " What is the smallest planet in our solar system? ",
 	answerList: ["Venus", "Pluto", "Mercury", "Mars"],
 	answer: 2
 },{
-	question: "What planet in this solar system is most suitable for life",
+	question: "What planet in this solar system is most suitable for life ?",
 	answerList: ["Neptune", "Midgard", "Mars", "Earth"],
 	answer: 3
 },{
-    question: "What roots did the name of the planets come from? ",
+    question: "What roots did the name of the planets come from ? ",
 	answerList: ["Greek", "Roman", "Egpytian", "Buddist"],
     answer: 1
 
@@ -23,8 +23,27 @@ var triviaQuestions = [{
     question: "Which planet was stripped of its name and is no longer a planet in 2006? ",
 	answerList: ["Pluto", "Mercury", "Uranus", "Earth"],
     answer: 0
+
+},{
+    question: " What planet is closest to the Sun ? ",
+	answerList: ["Venus", "Mercury", "Neptune", "Pluto"],
+    answer: 1
+
+},{
+    question: " What is the second smallest planet in the solar system ?  ",
+	answerList: ["Uranus", "Mercury", "Mars", "Saturn"],
+    answer: 2
     
-    
+},{
+    question: " Name the only planet in the Solar System to have one moon. ?  ",
+	answerList: ["Pluto", "Earth", "Mars", "Neptune"],
+    answer: 1
+
+},{
+    question: " What does the name of our planet Earth mean?  ",
+	answerList: ["The Soil Itself", "Pangea", "Dirt With Water", "Mother Nature"],
+    answer: 0
+     
 }];
 
 var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
@@ -101,6 +120,52 @@ function showCountdown(){
 		answerPage();
 	}
 }
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	
+	//checks to see if correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 2000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 2000);
+    }
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
+
+
 
 	
 
