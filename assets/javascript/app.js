43c629796b4633a26c1fa00cca4b332f9117b3b5
Steven <yoeung@outlook.com>
2018-06-16 01:09:31 -0400

    Final Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -104,7 +104,7 @@ function newQuestion(){
 }
 
 function countdown(){
-	seconds = 15;
+	seconds = 10;
 	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
 	answered = true;
 	//sets timer to go down
@@ -145,10 +145,10 @@ function answerPage(){
 	}
 	
 	if(currentQuestion == (triviaQuestions.length-1)){
-		setTimeout(scoreboard, 2000)
+		setTimeout(scoreboard, 1300)
 	} else{
 		currentQuestion++;
-		setTimeout(newQuestion, 2000);
+		setTimeout(newQuestion, 1300);
     }
 }
 
